51b6b76dc841d6248f91baa7f0faae2b7cf7aab8
ragan <raganalemu3@gmail.com>
2023-02-09 17:44:30 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const PORT = process.env.PORT || 3000;
+
+const io = require("socket.io")(PORT, {
+  cors: { origin: "*" },
+});
+const users = {};
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
